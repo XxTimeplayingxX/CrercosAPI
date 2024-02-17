556abaeae28e14b05dfc149d65fc6beb21a94228
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2024-02-16 21:15:46 -0500

    Add DbContext

---
+++ CrercosAPÍ/DATA/CrecosDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CrercosAPÍ.MODELS;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrercosAPÍ.DATA
+{
+    public class CrecosDbContext : DbContext //Heredamos la propiedad DbContext
+    {
+        public CrecosDbContext()
+        {
+            
+        }
+        public DbSet<Cliente> Clientes { get; set; } //DbSet son las tablas
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalle> PedidoDetalles { get; set; }
+       public DbSet<Producto> Productos { get; set; }
+    }
+}
--- CrercosAPÍ/Program.cs
+++ CrercosAPÍ/Program.cs
@@ -1,3 +1,7 @@
+using CrercosAPÍ.DATA;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext <CrecosDbContext>(options=> options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog =TiendaDB"));
 
 var app = builder.Build();
 
